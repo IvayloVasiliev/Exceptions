3e643a6f3fa2f2ac16efd2ef37c9f4575f3ae768
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-12-25 20:46:16 +0200

    Add project files.

---
+++ ConvertToDouble/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConvertToDouble
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var number = ReadDouble();
+            Console.WriteLine(number);
+        }
+
+        private static double ReadDouble()
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                var number = Convert.ToDouble(input);
+                return number;
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
